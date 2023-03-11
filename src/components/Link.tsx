ad21042f734d87797f98cb5054e798a6b87a4b22
YuzeTT <ziv1024@163.com>
2023-03-11 17:11:09 +0800

    feat: update

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,5 +1,5 @@
 export default function Link(props:any) {
   return (
-    <a href={props.href} target="_blank" class="relative after:absolute after:-left-1 after:-right-1 after:-z-10 after:top-2/3 after:bottom-0 after:bg-emerald-200 after:transition-all hover:after:top-0 mx-1" >{props.name}</a>
+    <a href={props.href} target="_blank" class="relative after:absolute after:-left-1 after:-right-1 after:-z-10 after:top-2/3 after:bottom-0 after:bg-orange-200 after:transition-all hover:after:top-0 mx-1" >{props.name}</a>
   )
 }
